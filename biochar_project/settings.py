f60878205c75d46f20b8d9751f224fd7eef16f40
Shivani Dubey <57933836+Shivanidubey23@users.noreply.github.com>
2025-08-04 13:10:06 +0530

    Update settings.py

--- biochar_project/settings.py
+++ biochar_project/settings.py
@@ -145,12 +145,7 @@ REST_FRAMEWORK = {
 }
 
 # CORS Configuration
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000,http://127.0.0.1:3000').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
